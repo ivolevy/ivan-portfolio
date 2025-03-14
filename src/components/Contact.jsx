745a1362207663670118de8c844ae1dc529369ae
ivan levy <ivo.levy03@gmail.com>
2025-03-14 16:55:52 -0300

    new form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -34,121 +34,121 @@ export const Contact = () => {
         "service_3hpiy2n", // Service ID
         "template_gvukjc5", // Template ID
         templateParams,
-        "E4xuiiz13SZoQSqQf" // Public Key
+        "H1IHW-E4xuiiz13SZoQSqQf" // Public Key
       )
       .then(
         () => {
-          setStatus("Message sent successfully!");
+          setStatus("Message sent successfully");
           setFormData({ name: "", company: "", email: "", message: "" });
         },
         (error) => {
-          setStatus("An error occurred, please try again.");
-          console.error("Error sending message:", error);
+          setStatus("Error sending message, please try again.");
+          console.error("Error sending:", error);
         }
       );
   };
 
   return (
     <>
-      <h2 className="servicesTitle text-center mb-5">Contact</h2>
-      <div className="flex flex-col items-center lg:flex-row mx-auto text-center contactContainer" id="contact">
-        
-        {/* Left Side: Contact Info + Social Media */}
-        <div className="lg:w-1/2 flex flex-col items-start justify-center text-left p-6 responsiveData">
-          <h3 className="text-3xl font-bold text-white contactSubtitle">Don’t be shy, let’s talk!</h3>
+    <h2 className="servicesTitle text-center mb-5">
+        Contact
+      </h2>
+    <div className="flex flex-col items-center lg:flex-row mx-auto text-center contactContainer" id="contact">
+      {/* Left side: Information + Social Media */}
+      <div className="lg:w-1/2 flex flex-col items-start justify-center text-left p-6 responsiveData">
+        <h3 className="text-3xl font-bold text-white contactSubtitle">Don’t be shy, let’s talk!</h3>
 
-          {/* Social Media Links */}
-          <div className="flex items-center space-x-4 mb-4">
-            <a 
-              href="https://www.instagram.com/ivo.levy" 
-              target="_blank" rel="noopener noreferrer" 
-              className="text-pink-500 hover:text-pink-600 text-3xl" 
-              aria-label="Instagram" title="Instagram"
-            >
-              <AiFillInstagram className="contactIcon"/>
-            </a>
-            <a 
-              href="https://www.linkedin.com/in/ivan-levy/" 
-              target="_blank" rel="noopener noreferrer" 
-              className="text-blue-700 hover:text-blue-800 text-3xl"
-              aria-label="LinkedIn" title="LinkedIn"
-            >
-              <TbBrandLinkedinFilled className="contactIcon"/>
-            </a>
-            <a 
-              href="https://wa.me/1138240929" 
-              target="_blank" rel="noopener noreferrer" 
-              className="text-green-500 hover:text-green-600 text-3xl"
-              aria-label="WhatsApp" title="WhatsApp"
-            >
-              <FaWhatsapp className="contactIcon"/>
-            </a>
-          </div>
-
-          {/* Email */}
-          <p className="text-gray-600">
-            <a href="mailto:ivo.levy03@gmail.com" className="text-white contactEmail">ivo.levy03@gmail.com</a>
-          </p>
-
+        {/* Social Media */}
+        <div className="flex items-center space-x-4 mb-4">
+          <a 
+            href="https://www.instagram.com/ivo.levy" 
+            target="_blank" rel="noopener noreferrer" 
+            className="text-pink-500 hover:text-pink-600 text-3xl" 
+            aria-label="Instagram" title="Instagram"
+          >
+            <AiFillInstagram className="contactIcon"/>
+          </a>
+          <a 
+            href="https://www.linkedin.com/in/ivan-levy" 
+            target="_blank" rel="noopener noreferrer" 
+            className="text-blue-700 hover:text-blue-800 text-3xl"
+            aria-label="Linkedin" title="Linkedin"
+          >
+            <TbBrandLinkedinFilled className="contactIcon"/>
+          </a>
+          <a 
+            href="https://wa.me/1138240929" 
+            target="_blank" rel="noopener noreferrer" 
+            className="text-green-500 hover:text-green-600 text-3xl"
+            aria-label="Whatsapp" title="Whatsapp"
+          >
+            <FaWhatsapp className="contactIcon"/>
+          </a>
         </div>
 
-        {/* Right Side: Contact Form */}
-        <form 
-          onSubmit={handleSubmit} 
-          className="lg:w-1/2 bg-white p-6 roundedForm responsiveForm shadow-md flex flex-col items-center text-center"
-        >
-          <div className="flex flex-col md:flex-row gap-4 mb-4 w-full">
-            <input
-              type="text"
-              name="name"
-              placeholder="Full Name"
-              value={formData.name}
-              onChange={handleChange}
-              required
-              className="w-full md:w-1/2 p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              aria-label="Full Name"
-            />
-            <input
-              type="text"
-              name="company"
-              placeholder="Company Name"
-              value={formData.company}
-              onChange={handleChange}
-              required
-              className="w-full md:w-1/2 p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
-              aria-label="Company Name"
-            />
-          </div>
+        {/* Email */}
+        <p className="text-gray-600 ">
+          <a href="mailto:ivo.levy03@gmail.com" className="text-white contactEmail">ivo.levy03@gmail.com</a>
+        </p>
 
-          <input
-            type="email"
-            name="email"
-            placeholder="Your Email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full p-3 mb-4 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            aria-label="Email"
-          />
-
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-            rows="4"
-            className="w-full p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
-            aria-label="Message"
-          ></textarea>
-
-          <button type="submit" className="contactButton mt-3">
-            Send Message
-          </button>
-
-          {status && <p className="text-green-600 text-center mt-3">{status}</p>}
-        </form>
+        {/* Schedule a Call */}
+        
       </div>
+
+      {/* Right side: Form */}
+      <form onSubmit={handleSubmit} className="lg:w-1/2 bg-white p-6 roundedForm responsiveForm shadow-md flex flex-col items-center text-center">
+        <div className="flex flex-col md:flex-row gap-4 mb-4 w-full">
+          <input
+            type="text"
+            name="name"
+            placeholder="Your name"
+            value={formData.name}
+            onChange={handleChange}
+            required
+            className="w-full md:w-1/2 p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            aria-label="Name"
+          />
+          <input
+            type="text"
+            name="company"
+            placeholder="Your Company"
+            value={formData.company}
+            onChange={handleChange}
+            required
+            className="w-full md:w-1/2 p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
+            aria-label="Company"
+          />
+        </div>
+
+        <input
+          type="email"
+          name="email"
+          placeholder="Your Email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+          className="w-full p-3 mb-4 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          aria-label="Email"
+        />
+
+        <textarea
+          name="message"
+          placeholder="Tell us about your case"
+          value={formData.message}
+          onChange={handleChange}
+          required
+          rows="4"
+          className="w-full p-3 border border-gray-300 roundedForm focus:outline-none focus:ring-2 focus:ring-blue-500"
+          aria-label="Message"
+        ></textarea>
+
+        <button type="submit" className="contactButton mt-3">
+          Send Message
+        </button>
+
+        {status && <p className="text-green-600 text-center mt-3">{status}</p>}
+      </form>
+    </div>
     </>
   );
 };
